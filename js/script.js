9f45330ca721934bea762f00489656a1f540a8f1
Sergey Voytsehovskiy <voytsehovsky90@gmail.com>
2023-03-05 17:48:03 +0200

    automatisation and valid answers

--- js/script.js
+++ js/script.js
@@ -10,12 +10,29 @@ const personalMovieDB = {
 	privat: false
 };
 
-const a = prompt("Однин з останніх переглянутих фільмів", ""),
-	  b = prompt("Дайте йому оцінку", ""),
-	  c = prompt("Однин з останніх переглянутих фільмів", ""),
-	  d = prompt("Дайте йому оцінку", "");
+for (let i = 0; i < 2; i++) {
+	const a = prompt("Однин з останніх переглянутих фільмів", ""),
+		  b = prompt("Дайте йому оцінку", "");
+	
+	if (a != null && b !== null && a != "" && b != "" && a.length < 50 ) {
+		console.log("done");
+		personalMovieDB.movies[a] = b;
+	} else {
+		console.log("error");
+		i--;
+	}
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+if (personalMovieDB.count < 10) {
+	console.log("Мало фільмів");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+	console.log("Класичний глядач");
+} else if (personalMovieDB.count > 30) {
+	console.log("Кіноман");
+} else {
+	console.log("Помилка");
+}
 
 console.log(personalMovieDB);
+
+
